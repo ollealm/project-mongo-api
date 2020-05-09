cc396ad06847554ac47bec4bb6136a5ef81e30bc
Olle Alm <olle.alm@me.com>
2020-05-09 22:52:58 +0200

    initial commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ import mongoose from 'mongoose'
 // import netflixData from './data/netflix-titles.json'
 // import topMusicData from './data/top-music.json'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/books"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
@@ -27,10 +27,92 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
+
+const Author = mongoose.model('Author', {
+  name: String,
 })
+
+const Book = mongoose.model('Book', {
+  title: String,
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Author'
+  }
+})
+
+if (process.env.RESET_DATABASE) {
+  console.log('Database reset')
+  const seedDatabase = async () => {
+    await Author.deleteMany()
+    await Book.deleteMany()
+
+    const tolkien = new Author({ name: 'J.R.R Tolkien' })
+    await tolkien.save()
+
+    const rowling = new Author({ name: 'J.K. Rowling' })
+    await rowling.save()
+
+    await new Book({ title: 'Hobbit', author: tolkien }).save()
+    await new Book({ title: 'Lord of the Rings', author: tolkien }).save()
+
+    await new Book({ title: 'Harry', author: rowling }).save()
+    await new Book({ title: 'Potter', author: rowling }).save()
+
+  }
+  seedDatabase()
+}
+
+// Animal.deleteMany().then(() => {
+
+//   new Animal({ name: 'Olle', age: 36, isFurry: false }).save()
+//   new Animal({ name: 'Olle2', age: 26, isFurry: true }).save()
+
+// })
+
+// Start defining your routes here
+// app.get('/', (req, res) => {
+//   Animal.find().then(animals => {
+//     res.json(animals)
+//   })
+//   //res.send('Hello world')
+// })
+
+
+app.get('/authors', async (req, res) => {
+  const authors = await Author.find()
+  res.json(authors)
+})
+
+app.get('/authors/:id', async (req, res) => {
+  const author = await Author.findById(req.params.id)
+  res.json(author)
+})
+
+app.get('/authors/:id/books', async (req, res) => {
+  const author = await Author.findById(req.params.id)
+  if (author) {
+    const books = await Book.find({ author: mongoose.Types.ObjectId(author.id) })
+    res.json(books)
+  } else {
+    res.status(404).json({ error: "Author not found" })
+  }
+})
+
+app.get('/books', async (req, res) => {
+  const books = await Book.find().populate('author')
+  res.json(books)
+})
+
+
+
+// Animal.findOne({ name: req.params.name }).then(animal => {
+//   if (animal) {
+//     res.json(animal)
+//   } else {
+//     res.status(404).json({ error: 'Not found' })
+//   }
+// })
+
 
 // Start the server
 app.listen(port, () => {
